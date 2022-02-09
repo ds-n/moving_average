0fc0d9a429b336e0d06aa41b5f13c06b2e69ca40
nicolas <ndesimone@sqli.com>
2022-02-09 14:04:53 +0100

    Weighted moving average

---
+++ test_wma.py
@@ -0,0 +1,14 @@
+import unittest
+import math
+from wma import wma
+
+
+class TestSimpleMovingAverage(unittest.TestCase):
+
+    def test_wma(self):
+        res_v1 = wma([1, 2, 3, 7, 9])
+        self.assertEqual(res_v1, [math.nan, math.nan, 1.0, 1.0, 1.0555555555555556])
+
+
+if __name__ == '__main__':
+    unittest.main()
--- wma.py
+++ wma.py
@@ -5,17 +5,14 @@
 # Everyone is permitted to copy and distribute verbatim copies
 # of this license document, but changing it is not allowed.
 # ==============================================================================
-"""Functions for calculate Simple moving average
+"""Functions for calculate Weighted moving average
 https://en.wikipedia.org/wiki/Moving_average
 """
-
 import math
 
 
-def sma(vec, k=3):
-    """Simple moving average."""
-    # window size
-    # Initialize an empty list to store moving averages
+def wma(vec, k=3):
+    """Weighted moving average."""
     moving_averages = []
 
     for _ in range(k - 1):
@@ -24,29 +21,11 @@ def sma(vec, k=3):
     # n is current position in data points
     n = k
 
+    # FIXME ?? calculating the WMA across successive values using Numerator_M
+    # See https://en.wikipedia.org/wiki/Moving_average section Weighted moving average
     while n <= len(vec):
-        sma = sum(vec[n - k:n]) / k
+        sma = sum(vec[n - k:n]) / sum(range(n - k, n)) / 2
         moving_averages.append(sma)
         n = n + 1
 
     return moving_averages
-
-
-def sma_v2(vec, k=3):
-    """Simple moving average v2."""
-    smak = []
-
-    for _ in range(k - 1):
-        smak.append(math.nan)
-
-    n = k - 1
-    while n < len(vec):
-        if math.isnan(smak[len(smak) - 1]):
-            sma = sum(vec[n + 1 - k:n + 1]) / k
-        else:
-            prev = smak[len(smak) - 1]
-            sma = prev + ((vec[n] - vec[n - k]) / k)
-        smak.append(sma)
-        n = n + 1
-
-    return smak
