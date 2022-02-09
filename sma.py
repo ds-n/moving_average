d449dbe543934e69a706d126acfb1e4ee5db8b50
nicolas <ndesimone@sqli.com>
2022-02-09 12:50:42 +0100

    tests sma

--- sma.py
+++ sma.py
@@ -1,24 +1,9 @@
-# The MIT License (MIT)
+# GNU GENERAL PUBLIC LICENSE
+# Version 3, 29 June 2007
 #
-# Copyright (c) 2015 nicolads87
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+# Everyone is permitted to copy and distribute verbatim copies
+# of this license document, but changing it is not allowed.
 # ==============================================================================
 """Functions for calculate Simple moving average
 https://en.wikipedia.org/wiki/Moving_average
@@ -63,12 +48,3 @@ def sma_v2(vec, k=3):
         n = n + 1
 
     return smak
-
-
-# test
-
-res_v1 = sma([1, 2, 3, 7, 9])
-assert res_v1 == [math.nan, math.nan, 2.0, 4.0, 6.333333333333333]
-
-res_v2 = sma_v2([1, 2, 3, 7, 9])
-assert res_v2 == [math.nan, math.nan, 2.0, 4.0, 6.333333333333334]
---
+++ test_sma.py
@@ -0,0 +1,18 @@
+import unittest
+import math
+from sma import sma, sma_v2
+
+
+class TestSimpleMovingAverage(unittest.TestCase):
+
+    def test_sma(self):
+        res_v1 = sma([1, 2, 3, 7, 9])
+        self.assertEqual(res_v1, [math.nan, math.nan, 2.0, 4.0, 6.333333333333333])
+
+    def test_sma_v2(self):
+        res_v2 = sma_v2([1, 2, 3, 7, 9])
+        self.assertEqual(res_v2, [math.nan, math.nan, 2.0, 4.0, 6.333333333333334])
+
+
+if __name__ == '__main__':
+    unittest.main()
